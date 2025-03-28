e8a3c3bc9d0c2c93aa79f6f64ae2d40c725b5845
Dalso13 <flskwhdhk@gmail.com>
2025-03-28 13:25:21 +0900

    feat: 회원가입 구현

---
+++ app/src/main/java/com/jdw/mango_contents/JoinActivity.kt
@@ -0,0 +1,49 @@
+package com.jdw.mango_contents
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+
+class JoinActivity : AppCompatActivity() {
+
+    private lateinit var auth: FirebaseAuth
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_join)
+
+        val btn = findViewById<Button>(R.id.joinBtn)
+        btn.setOnClickListener {
+            auth = Firebase.auth
+            val email = findViewById<EditText>(R.id.email).text.toString()
+            val pw = findViewById<EditText>(R.id.password).text.toString()
+
+            auth.createUserWithEmailAndPassword(email, pw)
+                .addOnCompleteListener(this) { task ->
+                    if (task.isSuccessful) {
+                        // Sign in success, update UI with the signed-in user's information
+                        val user = auth.currentUser
+                        startActivity(Intent(this, MainActivity::class.java))
+                        finish()
+                    } else {
+                        // If sign in fails, display a message to the user.
+                        Toast.makeText(
+                            baseContext,
+                            "Authentication failed.",
+                            Toast.LENGTH_SHORT,
+                        ).show()
+                    }
+                }
+
+        }
+    }
+}
--- app/src/main/java/com/jdw/mango_contents/SplashActivity.kt
+++ app/src/main/java/com/jdw/mango_contents/SplashActivity.kt
@@ -7,15 +7,30 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class SplashActivity : AppCompatActivity() {
+
+    private lateinit var auth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
 
+        auth = Firebase.auth
+        val currentUser = auth.currentUser
+
         Handler().postDelayed({
-            startActivity(Intent(this, MainActivity::class.java))
-            finish()
+            if (currentUser != null) {
+                startActivity(Intent(this, MainActivity::class.java))
+                finish()
+            } else {
+                startActivity(Intent(this, JoinActivity::class.java))
+                finish()
+            }
+
         }, 3000)
     }
 }
